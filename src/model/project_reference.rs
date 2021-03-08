d75e68d82cbc81a72abaef5180363dcfe85731ea
Laurent Quérel <laurent.querel@gmail.com>
2021-03-07 19:01:31 -0800

    Fix rust fmt issue

--- src/model/project_reference.rs
+++ src/model/project_reference.rs
@@ -1,9 +1,6 @@
-
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ProjectReference {
-	/// [Required] ID of the project. Can be either the numeric ID or the assigned ID of the project.
-	pub project_id: String,
+    /// [Required] ID of the project. Can be either the numeric ID or the assigned ID of the project.
+    pub project_id: String,
 }
-
-
