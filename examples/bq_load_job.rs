4e8c37edc20c71104a09aa7e5c1218a9a11b4d45
lquerel <laurent.querel@gmail.com>
2021-10-31 22:55:54 -0700

    Fix code fmt

--- examples/bq_load_job.rs
+++ examples/bq_load_job.rs
@@ -1,17 +1,16 @@
-/// This example explains how to initiate and supervise a BQ load job (new line delimited json file in GCS).
-
-use std::env;
-use std::thread::sleep;
-use std::time::Duration;
+use cloud_storage::Object;
 use gcp_bigquery_client::model::job::Job;
 use gcp_bigquery_client::model::job_configuration::JobConfiguration;
 use gcp_bigquery_client::model::job_configuration_load::JobConfigurationLoad;
 use gcp_bigquery_client::model::job_reference::JobReference;
-use gcp_bigquery_client::model::table_reference::TableReference;
-use log::error;
-use cloud_storage::Object;
-use gcp_bigquery_client::Client;
 use gcp_bigquery_client::model::job_status::JobStatus;
+use gcp_bigquery_client::model::table_reference::TableReference;
+use gcp_bigquery_client::Client;
+use log::error;
+/// This example explains how to initiate and supervise a BQ load job (new line delimited json file in GCS).
+use std::env;
+use std::thread::sleep;
+use std::time::Duration;
 
 const GCS_BUCKET_NAME: &'static str = "rust_bq_client";
 
@@ -23,8 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let tmp_file_name = tmp_file_name(30);
 
     // Load line delimiter json data file
-    let data = std::fs::read("examples/data.json")
-        .expect("data.json not found");
+    let data = std::fs::read("examples/data.json").expect("data.json not found");
 
     // Store data on GCS
     let source_uri = store_and_get_gcs_uri(GCS_BUCKET_NAME, data, &tmp_file_name).await?;
@@ -33,9 +31,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Create BQ load job to create/update the test with the content of the json data file
     // Pre-requisite: test_batch_load dataset already created
-    let job_ref = create_bq_load_job(&client, &project_id, "test_batch_load", "test4", source_uri, &tmp_file_name).await?;
+    let job_ref = create_bq_load_job(
+        &client,
+        &project_id,
+        "test_batch_load",
+        "test4",
+        source_uri,
+        &tmp_file_name,
+    )
+    .await?;
 
-    while get_job_status(&client, &project_id, job_ref.job_id.as_ref().unwrap()).await?.state != Some("DONE".to_string()) {
+    while get_job_status(&client, &project_id, job_ref.job_id.as_ref().unwrap())
+        .await?
+        .state
+        != Some("DONE".to_string())
+    {
         sleep(Duration::from_secs(1));
     }
 
@@ -59,11 +69,7 @@ pub async fn create_bq_load_job(
                 autodetect: Some(true),
                 // Never allow this job to create new tables.
                 create_disposition: Some("CREATE_IF_NEEDED".to_string()),
-                destination_table: Some(TableReference::new(
-                    project_id,
-                    dataset_id,
-                    table_id,
-                )),
+                destination_table: Some(TableReference::new(project_id, dataset_id, table_id)),
                 // Default to JSON for now
                 json_extension: None,
                 // None = no bad records are allowed.
@@ -82,10 +88,7 @@ pub async fn create_bq_load_job(
         ..Default::default()
     };
 
-    let job = client
-        .job()
-        .insert(project_id, job)
-        .await?;
+    let job = client.job().insert(project_id, job).await?;
 
     Ok(job.job_reference.expect("job_reference not found"))
 }
@@ -95,10 +98,7 @@ pub async fn get_job_status(
     project_id: &str,
     job_id: &str,
 ) -> Result<JobStatus, Box<dyn std::error::Error>> {
-    let job = client
-        .job()
-        .get_job(project_id, job_id, None)
-        .await?;
+    let job = client.job().get_job(project_id, job_id, None).await?;
 
     Ok(job.status.expect("job_status not found"))
 }
@@ -110,27 +110,24 @@ fn tmp_file_name(file_name_len: usize) -> String {
     let mut rng = rand::thread_rng();
 
     (0..file_name_len)
-        .map(|_| { CHARSET[rng.gen_range(0..CHARSET.len())] as char })
+        .map(|_| CHARSET[rng.gen_range(0..CHARSET.len())] as char)
         .collect()
 }
 
-pub async fn store_and_get_gcs_uri(gcs_bucket_name: &str, data: Vec<u8>, file_name: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let object = Object::create(
-        gcs_bucket_name,
-        data,
-        file_name,
-        "application/text",
-    ).await?;
+pub async fn store_and_get_gcs_uri(
+    gcs_bucket_name: &str,
+    data: Vec<u8>,
+    file_name: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let object = Object::create(gcs_bucket_name, data, file_name, "application/text").await?;
 
     Ok(format!("gs://{}/{}", object.bucket, object.name))
 }
 
 pub fn env_vars() -> (String, String) {
-    let project_id = env::var("PROJECT_ID")
-        .expect("Environment variable PROJECT_ID");
-    let gcp_sa_key = env::var("GOOGLE_APPLICATION_CREDENTIALS")
-        .expect("Environment variable GOOGLE_APPLICATION_CREDENTIALS");
+    let project_id = env::var("PROJECT_ID").expect("Environment variable PROJECT_ID");
+    let gcp_sa_key =
+        env::var("GOOGLE_APPLICATION_CREDENTIALS").expect("Environment variable GOOGLE_APPLICATION_CREDENTIALS");
 
     (gcp_sa_key, project_id)
 }
-
