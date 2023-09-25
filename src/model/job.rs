b5af23aec59d16c57619ec46ac80db3b8bdb0030
Shirlo <shirly@pelanor.io>
2023-09-25 18:48:02 +0300

    job-list-jobs: Fix user_email field in schemas

--- src/model/job.rs
+++ src/model/job.rs
@@ -5,11 +5,10 @@ use crate::model::job_status::JobStatus;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Job {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub configuration: Option<JobConfiguration>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", rename = "jobReference")]
     pub job_reference: Option<JobReference>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub statistics: Option<JobStatistics>,
@@ -27,9 +26,12 @@ pub struct Job {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub kind: Option<String>,
     /// [Output-only] A URL that can be used to access this resource again.
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", rename = "selfLink")]
     pub self_link: Option<String>,
     /// [Output-only] Email address of the user who ran the job.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub user_email: Option<String>,
+    /// [Output-only] String representation of identity of requesting party. Populated for both first- and third-party identities. Only present for APIs that support third-party identities.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub principal_subject: Option<String>,
 }
