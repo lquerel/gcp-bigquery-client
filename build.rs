5f2c7ffda3c0738ba8c16bfffc6613ed2d720c89
Raminder Singh <romi_ssk@yahoo.co.in>
2024-07-08 12:44:08 +0530

    fix formatting

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile(
-            &["googleapis/google/cloud/bigquery/storage/v1/storage.proto"],
-            &["googleapis"],
-        )?;
+    tonic_build::configure().build_server(false).compile(
+        &["googleapis/google/cloud/bigquery/storage/v1/storage.proto"],
+        &["googleapis"],
+    )?;
     Ok(())
 }
