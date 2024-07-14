e7cd5e8cbce32a10b52a619a88f6a45057de7767
Laurent Querel <laurent.querel@gmail.com>
2024-07-14 10:53:47 -0700

    chore(build): Remove dependency on protoc

--- build.rs
+++ build.rs
@@ -1,6 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // The gRPC Storage Write API is vendored in `src/google` to avoid
+    // depending on protoc in GitHub Actions.
+    //
+    // To regenerate the gRPC Storage Write API from the proto file:
+    // - Comment the build_transport(false) line below.
+    // - Run `cargo build` to regenerate the API.
+    // - Uncomment the build_transport(false) line below.
+    // - Commit the changes.
     tonic_build::configure()
+        .build_transport(false)
         .build_server(false)
+        .out_dir("src/google")
         .compile(
             &["googleapis/google/cloud/bigquery/storage/v1/storage.proto"],
             &["googleapis"],
